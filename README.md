8f2c625cb650ceefc0b6446695658cf29fee6938
cesami <149131463+cesami@users.noreply.github.com>
2024-01-14 23:55:40 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Criando um Ransonware com Python
+
+
+### Resutados
+
+Pessoal fiz umas pesquenas personalizações nos arquivos encrypter.py, decrypter.py e teste1.txt
+
+ 
+
+
--- decrypter.py
+++ decrypter.py
@@ -2,7 +2,7 @@ import os
 import pyaes
 
 ## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
+file_name = "teste1.txt.ransomware"
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
@@ -15,8 +15,10 @@ decrypt_data = aes.decrypt(file_data)
 ## remover o arquivo criptografado
 os.remove(file_name)
 
+print("O arquivo sera descriptografado !")
+
 ## criar o arquivo descriptografado
-new_file = "teste.txt"
+new_file = "teste1.txt"
 new_file = open(f'{new_file}', "wb")
 new_file.write(decrypt_data)
 new_file.close()
